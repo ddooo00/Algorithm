22ca152972e1d475256f75871d9f540282a1cc18
ddooo00 <122673560+ddooo00@users.noreply.github.com>
2023-11-21 22:32:10 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
@@ -1,11 +1,11 @@
 function solution(arr1, arr2) {
-    let answer;
-    if (arr1.length == arr2.length) {
-        let sum1 = arr1.reduce((sum, cur) => sum + cur, 0);
-        let sum2 = arr2.reduce((sum, cur) => sum + cur, 0);
-        answer = sum1 == sum2 ? 0 : (sum1 > sum2 ? 1 : -1);
-    } else {
-        answer = arr1.length > arr2.length ? 1 : -1;
+    if(arr1.length === arr2.length){
+        const arr1Sum = arr1.reduce((acc, curr)=> acc+curr);
+        const arr2Sum = arr2.reduce((acc, curr)=> acc+curr);
+
+        return arr1Sum === arr2Sum ? 0: arr1Sum > arr2Sum  ? 1: -1 ;
+    }else{
+        return arr1.length > arr2.length ? 1 : -1;
+
     }
-    return answer;
 }
