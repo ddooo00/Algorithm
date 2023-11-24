c1a27501fdbf0183ab8795bf006d00dbbe961878
ddooo00 <122673560+ddooo00@users.noreply.github.com>
2023-11-24 20:18:07 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,9 +1,8 @@
 function solution(n) {
-    let arr = [];
-    for(let i = 1; i <= n; i++) 
-    if(n % i === 0) {
-        arr.push(n / i);
+    for (let i = 0; i < n/2; i++) {
+        if (i*i == n) {
+            return 1;
+        }
     }
-    return arr.length % 2 ? 1 : 2;
+    return 2;
 }
-//제곱수 -> 나머지 값이 없는 값
