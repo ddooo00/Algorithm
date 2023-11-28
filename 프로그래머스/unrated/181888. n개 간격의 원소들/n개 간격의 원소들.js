23f4567e5275223e391ba956d83f685f44099e93
ddooo00 <122673560+ddooo00@users.noreply.github.com>
2023-11-29 00:00:03 +0900

    [level unrated] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,7 +1,9 @@
 function solution(num_list, n) {
-    let list =[]
-    for(let i=0; i<num_list.length; i +=n){
-        list.push(num_list[i])
-    }
-    return list
+    let answer = [];
+
+     for(let i = 0; i<num_list.length; i+=n){
+         answer.push(num_list[i])
+     }
+
+    return answer;
 }
