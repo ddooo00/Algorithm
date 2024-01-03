cb495ca602f2acd143dfd9a69ac3436c8879e3de
ddooo00 <122673560+ddooo00@users.noreply.github.com>
2024-01-03 23:39:51 +0900

    [level unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -1,14 +1,1 @@
-function solution(num_list, n) {
-let answer = 0;
-
-    // 반복문을 사용하여 num_list 순회
-    for (let i = 0; i < num_list.length; i++) {
-        // 만약 n이 num_list 안에 있다면 answer를 1로 설정하고 반복문 종료
-        if (num_list[i] === n) {
-            answer = 1;
-            break;
-        }
-    }
-
-    return answer;
-}
+const solution = (num_list, n) => num_list.includes(n) ? 1 : 0
