79cf4877fc52817bca24b3782963f44c8e5a1990
ddooo00 <122673560+ddooo00@users.noreply.github.com>
2024-01-24 22:10:04 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,3 +1,11 @@
 function solution(my_string) {
-    return my_string.split('').map(n => n === n.toUpperCase() ? n.toLowerCase() : n.toUpperCase()).join('')
+    let answer = ''
+    for(let i of my_string){
+        if(i === i.toUpperCase()){
+            answer += i.toLowerCase();
+        }else{
+            answer += i.toUpperCase();
+        }
+    }
+    return answer;
 }
